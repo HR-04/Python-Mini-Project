87803e00c04601bd1fe9e071974fe6bc4926b6f8
Harini Rengarajan <110718731+HR-04@users.noreply.github.com>
2023-01-16 10:21:58 +0530

    corrected some Grammatical error.

--- Adventure_game.py
+++ Adventure_game.py
@@ -2,7 +2,7 @@ name = input("Type your name: ")
 print("Welcome", name, "to this adventure!")
 
 answer = input(
-    "You are in a spooky area searching the way out.would you like to take left or right? ").lower()
+    "You are in a spooky area searching for the way out.would you like to take left or right? ").lower()
 
 if answer == "left":
     answer = input(
@@ -11,24 +11,23 @@ if answer == "left":
     if answer == "on":
         print("YOU SAW A GHOST AND GOT POSSESSED!! ")
     elif answer == "off":
-        print("you trip into a sharp knife and died.")
+        print("you tripped into a sharp knife and died.")
     else:
         print('Not a valid option. You lose.')
 
 elif answer == "right":
-    answer = input(
-        "You come to a room entrance door, it looks shabby, do you want to pass it or head back (pass/back)? ").lower()
+    answer = input("You come to a room entrance door, it looks shabby, do you want to pass it or head back (pass/back)? ").lower()
 
     if answer == "back":
         print("You go back and lose.")
     elif answer == "pass":
         answer = input(
-            "You pass the room and meet a stranger. Do you talk to them (yes/no)? ").lower()
+            "You pass the room and meet a stranger. Do you want to talk to them (yes/no)? ").lower()
 
         if answer == "yes":
             print("You talk to the stanger and they give you map to find a way out. You WIN!")
         elif answer == "no":
-            print("You ignore the stranger and they are offended.You got snapped and lose!.")
+            print("You ignore the stranger and they got offended and murdered you!!.")
         else:
             print('Not a valid option. You lose.')
     else:
